6d55dc9b2d91fef96c457aba152466efe05daa0c
syntakks <syntakks@gmail.com>
2021-06-04 14:00:54 -0500

    Initial Commit

---
+++ SwiftUIFormValidation/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUIFormValidation
+//
+//  Created by Stephen Wall on 6/4/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUIFormValidation/SwiftUIFormValidationApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIFormValidationApp.swift
+//  SwiftUIFormValidation
+//
+//  Created by Stephen Wall on 6/4/21.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIFormValidationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
